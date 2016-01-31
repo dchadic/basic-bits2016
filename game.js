bfb1d7bce46b008252445f122975394eb98b7e6f
Damon Chadic <dchadic@ucsc.edu>
2016-01-30 20:48:08 -0800

    Implemented sprites

--- game.js
+++ game.js
@@ -45,7 +45,7 @@ function init(){
     images.spin.push(createImage("spin/"+i+".png"));
   }
   images.symbol = createImage("filler_symbol.png");
-  images.cats = [9];
+  images.cats = [];
   for (var i = 0; i <9; i++) {
      images.cats[i] = [4]
      for (var j = 1; j <= 4; j++) {
--- slots.js
+++ slots.js
@@ -9,9 +9,12 @@ var whichPic = parseInt(Math.random()*9);
 var slotOne = 0;
 var slotTwo = 0;
 var slotThree = 0;
+var frameCount = 0;
 
 // called each frame in the slots view
 function slotsUpdate(){
+  frameCount++;
+  frameCount = frameCount % 20;
   ctx.drawImage(images.slotsbackground, 0, 0);
   spinFrame = (spinFrame + 1) % 15;
   if(payoutRemaining > 0){
@@ -21,11 +24,11 @@ function slotsUpdate(){
   let x = payoutRemaining % 10;
   if(!(x==3 || x==4 || x==5)) ctx.fillText("Coins: " + coins, 4, 4); //blink coins text if there is still payout remaining
   if(reelsSpinning > 2) ctx.drawImage(images.spin[spinFrame], 90, 180);
-  else ctx.drawImage(images.cats[slotOne][1], 90, 180);
+  else ctx.drawImage(images.cats[slotOne][Math.floor(frameCount/5)], 90, 180);
   if(reelsSpinning > 1) ctx.drawImage(images.spin[(spinFrame+4)%15], 250, 180);
-  else ctx.drawImage(images.cats[slotTwo][1], 250, 180);
+  else ctx.drawImage(images.cats[slotTwo][Math.floor(frameCount/5)], 250, 180);
   if(reelsSpinning > 0) ctx.drawImage(images.spin[(spinFrame+8)%15], 410, 180);
-  else ctx.drawImage(images.cats[slotThree][1], 410, 180);
+  else ctx.drawImage(images.cats[slotThree][Math.floor(frameCount/5)], 410, 180);
 }
 
 // called when the mouse is moved
